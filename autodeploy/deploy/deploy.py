037e1933ea470ad4315e7eb0689bea61df82f5e0
gusseppe <gusseppebravo@gmail.com>
2019-10-07 08:38:49 +0200

    deploy changes

--- autodeploy/deploy/deploy.py
+++ autodeploy/deploy/deploy.py
@@ -17,7 +17,7 @@ client = docker.from_env()
 
 class Deploy:
     def __init__(self,
-                 platform):
+                 platform=None):
         """
         Example:
             deployer = Deploy(platform, workflow)
@@ -27,14 +27,19 @@ class Deploy:
             workflow (dict): Dict of python files, the workflow to run.
 
         """
-        self.platform = platform
-        self.containers = platform.containers
-        self.app_type = platform.app_type
-        self.workflow = platform.workflow
-        self.single_app_dir = platform.single_app_dir
+        if platform is not None:
+            self.platform = platform
+            self.containers = platform.containers
+            self.app_type = platform.app_type
+            self.workflow = platform.workflow
+            self.single_app_dir = platform.single_app_dir
+
         self.logs_workflow = None
         self.logs_build_image = None
         self.logs_run_ctn = None
+        self.api_image_name = None
+        self.api_container_name = None
+        self.api_container_object = None
 
     def pipeline(self):
         logging.info(f'Pipeline: run_workflow() and deploy() are running.')
@@ -78,6 +83,7 @@ class Deploy:
             logging.info(f' Deploying model as API: type={self.app_type} .')
             api_image_name = f'{name}_{self.app_type}_api'
             api_container_name = api_image_name
+
             models_path = os.path.join(self.single_app_dir, 'workflow', 'models')
 
             logging.info(f" Building image: {api_image_name}. ")
@@ -86,12 +92,43 @@ class Deploy:
             # logging.info(f" Output image: {logs_build_image} ")
             logging.info(f" Image: {api_image_name} was built successfully. ")
 
-            logging.info(f" Creating container: {api_container_name}. ")
-            cmd_run = f'docker run -d --name {api_container_name} -p  5001:8080 {api_image_name}'
-            logs_run_ctn = subprocess.check_output(cmd_run.split())
-            logging.info(f" Container: {api_container_name} was created successfully. ")
-            logging.info(f" API at: htpp://localhost:5001. ")
+            self.run_api(name='app', app_type='single')
+            # logging.info(f" Creating container: {api_container_name}. ")
+            # cmd_run = f'docker run -d --name {api_container_name} -p  5001:8080 {api_image_name}'
+            # logs_run_ctn = subprocess.check_output(cmd_run.split())
+            # logging.info(f" Container: {api_container_name} was created successfully. ")
+            # logging.info(f" API at: htpp://localhost:5001. ")
             # logging.info(f" Output container: {logs_run_ctn} ")
 
+            self.api_image_name = api_image_name
+            self.api_container_name = api_container_name
             self.logs_build_image = logs_build_image
-            self.logs_run_ctn = logs_run_ctn
+            # self.logs_run_ctn = logs_run_ctn
+
+    def run_api(self, name='app', app_type='single'):
+        """
+        Run the API model into a Docker container.
+
+        Parameters:
+            name (str): Prefix of a Docker container.
+            app_type (str): Type of the app to be deployed.
+        Returns:
+            image (object): Docker container.
+        """
+        if app_type == 'single':
+            api_image_name = f'{name}_{app_type}_api'
+            api_container_name = api_image_name
+            logging.info(f" Creating container: {api_container_name}. ")
+
+            ports = {'8000/tcp': 5001}
+            container = client.containers.run(api_image_name, name=api_container_name,
+                                              tty=True, detach=True,
+                                              ports=ports)
+
+            logging.info(f'API model is running as {api_container_name} container.')
+            # cmd_run = f'docker run -d --name {api_container_name} -p  5001:8080 {api_image_name}'
+            # logs_run_ctn = subprocess.check_output(cmd_run.split())
+            logging.info(f" Container: {api_container_name} was created successfully. ")
+            logging.info(f" API at: htpp://localhost:5001. ")
+
+            self.api_container_object = container
