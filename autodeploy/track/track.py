c27048cb417a5a6461c2f93fe1d2971331d17f73
gusseppebravo <gusseppebravo@gmail.com>
2019-10-24 23:19:45 +0200

    leaf demo added

--- autodeploy/track/track.py
+++ autodeploy/track/track.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 
 from scipy.stats import ks_2samp
 
-logging.basicConfig(format='%(asctime)s - %(message)s',
+logging.basicConfig(format='%(asctime)s -  %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 logging.getLogger().setLevel(logging.INFO)
 
@@ -27,7 +27,7 @@ client = docker.from_env()
 
 class Track:
     def __init__(self,
-                 api_name='app_single_api',
+                 # api_container_name='app_single_api',
                  port=5001,
                  path_saved_preds=None):
         """
@@ -35,11 +35,11 @@ class Track:
             deploy = Track(api_name, port)
 
         Parameters:
-            api_name (str): API's container name.
+            api_container_name (str): API's container name.
             port (int): API's port.
 
         """
-        self.api_name = api_name
+        # self.api_name = api_container_name
         self.port = port
         self.path_saved_preds = path_saved_preds
         self.input_to_predict = None
@@ -64,30 +64,37 @@ class Track:
         """
         url = f'http://localhost:{self.port}/invocations'
 
-        start = time.time()
-        response = requests.post(
-                     url=url, data=json.dumps(input_to_predict),
-                     headers={"Content-type": "application/json; format=pandas-split"})
-        response_json = json.loads(response.text)
+        try:
+            start = time.time()
+            response = requests.post(
+                         url=url, data=json.dumps(input_to_predict),
+                         headers={"Content-type": "application/json; format=pandas-split"})
+            response_json = json.loads(response.text)
 
-        end = time.time()
-        logging.info(f'Predicting from port: {self.port}')
-        logging.info(f'Time elapsed: {end-start}')
+            end = time.time()
+            logging.info(f'Predicting from port: {self.port}')
+            logging.info(f'Time elapsed: {end-start}')
 
-        self.input_to_predict = input_to_predict
-        self.predictions = response_json
+            self.input_to_predict = input_to_predict
+            self.predictions = response_json
 
-        preds = [d['0'] for d in self.predictions]
-        df_pred = pd.DataFrame(self.input_to_predict['data'],
-                               columns=self.input_to_predict['columns'])
-        df_pred['pred'] = preds
+            # preds = [d['0'] for d in self.predictions]
+            preds = [d for d in self.predictions]
+            df_pred = pd.DataFrame(self.input_to_predict['data'],
+                                   columns=self.input_to_predict['columns'])
+            df_pred['pred'] = preds
 
-        self.input_pred = df_pred
+            self.input_pred = df_pred
 
-        if to_save:
-            self.save_prediction(path_pred)
+            if to_save:
+                self.save_prediction(path_pred)
 
-        return df_pred
+            return df_pred
+        except requests.exceptions.HTTPError as e:
+            logging.error(f"{e}")
+            logging.error(f"Request to API failed.")
+
+
 
     def save_prediction(self, path_pred):
         """
@@ -101,7 +108,7 @@ class Track:
 
         path_pred = os.path.join(path_pred, 'predictions.csv')
         self.input_pred.to_csv(path_pred, index=False)
-        logging.info(f'Input an predictions were saved at: {path_pred}')
+        logging.info(f'Input and predictions were saved at: {path_pred}')
 
         # self.input_pred.to_csv(path_pred,
         #                        mode='a', index=False, header=False)
